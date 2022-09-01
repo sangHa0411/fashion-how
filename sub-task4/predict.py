f336b2e4e8a7067d5ba5adb95cc8ce5f196a487e
sangha0411 <sangha110495@gmail.com>
2022-09-01 10:43:20 +0000

    update predict.py

--- sub-task4/predict.py
+++ sub-task4/predict.py
@@ -21,8 +21,12 @@ TEXT_FEAT_SIZE = 512
 DROPOUT_PROB = 0.1
 MEM_SIZE = 24
 HOPS = 3
+NUM_LAYERS = 6
+D_MODEL = 512
+HIDDEN_SIZE = 2048
+NUM_HEAD = 8
 BATCH_SIZE = 8
-EVAL_NODE = '[6000,6000,6000,6000,512][2000,2000,2000]'
+EVAL_NODE = '[1024,4096,4096]'
 NUM_WORKERS = 2
 
 def convert(pred) :
@@ -51,11 +55,11 @@ def inference() :
 
     # -- Subword Embedding
     print("\nLoading Tokenizer")
-    swer = SubWordEmbReaderUtil("data/sstm_v0p5_deploy/sstm_v4p49_np_n36134_d128.dat")
+    swer = SubWordEmbReaderUtil("/home/work/data/task4/sstm_v0p5_deploy/sstm_v4p49_np_n36134_d128.dat")
 
     # -- Data
     print("\nLoading Dataset")
-    eval_diag_loader = DialogueTestLoader("data/fs_eval_t1.wst.dev")
+    eval_diag_loader = DialogueTestLoader("/home/work/data/task4/fs_eval_t1.wst.dev")
     eval_dataset = eval_diag_loader.get_dataset()
 
     # -- Model
@@ -68,6 +72,10 @@ def inference() :
         num_rnk=NUM_RNK,
         num_feature=NUM_FEATURE,
         num_cordi=NUM_CORDI,
+        num_layers=NUM_LAYERS,
+        d_model=D_MODEL,
+        hidden_size=HIDDEN_SIZE,
+        num_head=NUM_HEAD,
         dropout_prob=DROPOUT_PROB,
         text_feat_size=TEXT_FEAT_SIZE,
         img_feat_size=IMG_FEAT_SIZE
@@ -85,7 +93,7 @@ def inference() :
     print("\nFormatting torch dataset")
     data_collator = PaddingCollator()
     eval_torch_dataset = FashionHowDataset(dataset=eval_encoded_dataset, 
-        img_feat_dir="/home/wkrtkd911/project/fashion-how/sub-task4/data/img_feats"
+        img_feat_dir="/home/work/data/task4/img_feats"
     )
     eval_dataloader = DataLoader(eval_torch_dataset, 
         batch_size=BATCH_SIZE, 
@@ -119,7 +127,7 @@ def inference() :
         pred_ids.append(convert(pred))
 
     pred_id_array = np.array(pred_ids)
-    np.savetxt(f"results/prediction.csv", 
+    np.savetxt("/home/work/model/prediction.csv",
         pred_id_array.astype(int), 
         encoding='utf8', 
         fmt='%d'
